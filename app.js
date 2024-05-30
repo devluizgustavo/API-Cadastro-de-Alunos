511d4c25e7f9da0b4bf62ee1c572a50c1ed61285
Luiz Gustavo <luizgustavobonke@gmail.com>
2024-05-29 23:08:09 -0300

    Adicionando Dotenv

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-import express from 'express';
-import homeRoutes from './src/routes/homeRoutes';
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import homeRoutes from "./src/routes/homeRoutes";
 
 class App {
   constructor() {
@@ -14,7 +16,7 @@ class App {
   }
 
   routes() {
-    this.app.use('/', homeRoutes);
+    this.app.use("/", homeRoutes);
   }
 }
 
